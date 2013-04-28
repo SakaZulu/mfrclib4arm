984843d4ced55b94fbdbfbcca05bb67b73fb1448
Fer.villa9007@gmail.com <Fer.villa9007@gmail.com@1a7d4e6f-06d0-991c-f786-564482a8a819>
2013-04-28 02:20:09 +0000

    working RFID driver and terminal ftdi

--- trunk/PandaRFID/Panda_RFID/Panda_RFID/Program.cs
+++ trunk/PandaRFID/Panda_RFID/Panda_RFID/Program.cs
@@ -3,6 +3,8 @@ using System.Threading;
 
 using Microsoft.SPOT;
 using Microsoft.SPOT.Hardware;
+using System.IO.Ports;
+using System.Text;
 
 using GHIElectronics.NETMF.FEZ;
 
@@ -12,20 +14,27 @@ namespace Panda_RFID
     {
         public static void Main()
         {
-            // Blink board LED
-
-            bool ledState = false;
-
-            OutputPort led = new OutputPort((Cpu.Pin)FEZ_Pin.Digital.LED, ledState);
-
+            
+            SerialPort UART = new SerialPort("COM1", 9600);
+            SPI.Configuration config = new SPI.Configuration((Cpu.Pin)FEZ_Pin.Digital.Di10, false, 0, 0, true, true, 250, SPI.SPI_module.SPI1);
+            SPI Spi = new SPI(config);
+            UART.Open();
+            string Datoreaded = "";
+            byte[] datain = new byte[1];
+            byte[] dataout = new byte[1];
             while (true)
             {
                 // Sleep for 500 milliseconds
                 Thread.Sleep(500);
-
-                // toggle LED state
-                ledState = !ledState;
-                led.Write(ledState);
+                
+                dataout[0] = (byte)0x55;
+                datain[0] = (byte)0;
+                Spi.WriteRead(dataout, datain);
+                Datoreaded = datain[0].ToString();
+                Datoreaded += "\n\r";
+                byte[] buffer = Encoding.UTF8.GetBytes(Datoreaded);
+                UART.Write(buffer, 0, buffer.Length);
+                
             }
         }
 
