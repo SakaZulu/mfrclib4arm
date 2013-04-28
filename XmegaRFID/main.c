f459c6cafa74e354fb6a1fdc251fed0be9ab7b92
jlarevaloh <jlarevaloh@gmail.com>
2013-04-28 21:34:09 +0000

    SPI driver was added.

--- XmegaRFID/main.c
+++ XmegaRFID/main.c
@@ -14,6 +14,8 @@
 #include "task.h"
 #include "leds.h"
 #include "clksys_driver.h"
+#include "spi_driver.h"
+#include "spi_com.h"
 
 /* The number of coroutines to create. */
 #define mainNUM_FLASH_COROUTINES		( 3 )
@@ -26,11 +28,13 @@ static void task1( void *pvParameters );
 static void task2( void *pvParameters );
 
 unsigned char gFlag;
+extern SPI_Master_t spiMasterC;
 
 int main(void)
 {
 	leds_init();
 	osc_intern_32MHz_init();
+	spi_com_init();
 
 	gFlag = 0;
 	xTaskCreate( task1, ( signed char * ) "Task1", configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY+2), ( xTaskHandle * )NULL );
@@ -70,9 +74,45 @@ static portTASK_FUNCTION( task2, pvParameters )
 	/* Just to stop compiler warnings. */
 	( void ) pvParameters;
 
+	SPI_DataPacket_t myDataPacket;
+	#define buff_len	3
+	unsigned char buff_tx[buff_len];
+	unsigned char buff_rx[buff_len];
+	buff_tx[0] = 0x33;
+	buff_tx[1] = 0x33;
+	buff_tx[2] = 0x55;
+
+	/* Create data packet */
+	SPI_MasterCreateDataPacket(
+	&myDataPacket,
+	buff_tx,
+	buff_rx,
+	buff_len,
+	&PORTC,
+	PIN4_bm);
+	
+	unsigned char cTemp;
+			
 	for( ;; )
 	{
 		gFlag ^= (1<<0);
+		/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+			
+		/* Transmit and receive first data byte. */
+		uint8_t status;
+		do {
+			status = SPI_MasterInterruptTransceivePacket(&spiMasterC, &myDataPacket);
+			vTaskDelay(1);
+		} while (status != SPI_OK);
+		/* Wait for transmission to complete. */
+		while(myDataPacket.complete == false) {
+			vTaskDelay(1);
+		}
+		
+		cTemp = spiMasterC.module->DATA;
+		cTemp = 0x00;
+		buff_tx[2]++;
+		/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 		vTaskDelay(500);
 	}
 }
