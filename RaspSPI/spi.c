31112548e31a11a7759a12dd1533d70fe3d5df09
esanchez.briones <esanchez.briones@gmail.com>
2013-04-29 15:14:13 +0000

    Update :)

--- RaspSPI/spi.c
+++ RaspSPI/spi.c
@@ -1,16 +1,14 @@
 /*
- * SPI testing utility (using spidev driver)
- *
- * Copyright (c) 2007  MontaVista Software, Inc.
- * Copyright (c) 2007  Anton Vorontsov <avorontsov@ru.mvista.com>
+ * Created: 27/04/2013
+ * Author: esanchez.briones@gmail.com
+ * Hackaton!, http://www.hackathonmty.org/
+ * Description: Spi simple implementation Raspberry Pi 
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License.
+ * the Free Software Foundation; either version 3 of the License.
  *
- * Cross-compile with cross-gcc -I/path/to/cross-kernel/include
  */
-
 #include <stdint.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -85,9 +83,9 @@ int Init_SPI(const char * dev)
 	if (ret == -1)
 		pabort("can't get max speed hz");
 
-	printf("spi mode: %d\n", mode);
-	printf("bits per word: %d\n", bits);
-	printf("max speed: %d Hz (%d KHz)\n", speed, speed/1000);
+//	printf("spi mode: %d\n", mode);
+//	printf("bits per word: %d\n", bits);
+//	printf("max speed: %d Hz (%d KHz)\n", speed, speed/1000);
 
 	return fd;
 }
@@ -99,9 +97,6 @@ uint8_t SPI_transfer(int fd,uint8_t byte)
 	uint8_t tx[2];
 	memset(rx,0,sizeof(rx));
 	memset(tx,0,sizeof(tx));
-
-
-
 
 	tx[0]=byte;
 	tx[1]=0;
@@ -120,18 +115,8 @@ uint8_t SPI_transfer(int fd,uint8_t byte)
 	if (ret < 1)
 		pabort("can't send spi message");
 	rx_byte=rx[0];
-	//printf("Send %003d Rcv (%003d- %003d )\r\n",byte,rx[0],rx[1]);
-	//puts("");
-	//if(rx_byte)
-	//printf(" Read SPI [%02d] \r\n",rx_byte);
-
-
-
 	return rx_byte;
 }
-
-
-
 
 static void transfer(int fd)
 {
@@ -167,142 +152,5 @@ static void transfer(int fd)
 	puts("");
 }
 
-static void print_usage(const char *prog)
-{
-	printf("Usage: %s [-DsbdlHOLC3]\n", prog);
-	puts("  -D --device   device to use (default /dev/spidev1.1)\n"
-			"  -s --speed    max speed (Hz)\n"
-			"  -d --delay    delay (usec)\n"
-			"  -b --bpw      bits per word \n"
-			"  -l --loop     loopback\n"
-			"  -H --cpha     clock phase\n"
-			"  -O --cpol     clock polarity\n"
-			"  -L --lsb      least significant bit first\n"
-			"  -C --cs-high  chip select active high\n"
-			"  -3 --3wire    SI/SO signals shared\n");
-	exit(1);
-}
-/*
-static void parse_opts(int argc, char *argv[])
-{
-	while (1) {
-		static const struct option lopts[] = {
-				{ "device",  1, 0, 'D' },
-				{ "speed",   1, 0, 's' },
-				{ "delay",   1, 0, 'd' },
-				{ "bpw",     1, 0, 'b' },
-				{ "loop",    0, 0, 'l' },
-				{ "cpha",    0, 0, 'H' },
-				{ "cpol",    0, 0, 'O' },
-				{ "lsb",     0, 0, 'L' },
-				{ "cs-high", 0, 0, 'C' },
-				{ "3wire",   0, 0, '3' },
-				{ "no-cs",   0, 0, 'N' },
-				{ "ready",   0, 0, 'R' },
-				{ NULL, 0, 0, 0 },
-		};
-		int c;
 
-		c = getopt_long(argc, argv, "D:s:d:b:lHOLC3NR", lopts, NULL);
 
-		if (c == -1)
-			break;
-
-		switch (c) {
-		case 'D':
-			device = optarg;
-			break;
-		case 's':
-			speed = atoi(optarg);
-			break;
-		case 'd':
-			delay = atoi(optarg);
-			break;
-		case 'b':
-			bits = atoi(optarg);
-			break;
-		case 'l':
-			mode |= SPI_LOOP;
-			break;
-		case 'H':
-			mode |= SPI_CPHA;
-			break;
-		case 'O':
-			mode |= SPI_CPOL;
-			break;
-		case 'L':
-			mode |= SPI_LSB_FIRST;
-			break;
-		case 'C':
-			mode |= SPI_CS_HIGH;
-			break;
-		case '3':
-			mode |= SPI_3WIRE;
-			break;
-		case 'N':
-			mode |= SPI_NO_CS;
-			break;
-		case 'R':
-			mode |= SPI_READY;
-			break;
-		default:
-			print_usage(argv[0]);
-			break;
-		}
-	}
-}
- */
-//int main(int argc, char *argv[])
-//{
-//	int ret = 0;
-//	int fd;
-//
-//	parse_opts(argc, argv);
-//
-//	fd = open(device, O_RDWR);
-//	if (fd < 0)
-//		pabort("can't open device");
-//
-//	/*
-//	 * spi mode
-//	 */
-//	ret = ioctl(fd, SPI_IOC_WR_MODE, &mode);
-//	if (ret == -1)
-//		pabort("can't set spi mode");
-//
-//	ret = ioctl(fd, SPI_IOC_RD_MODE, &mode);
-//	if (ret == -1)
-//		pabort("can't get spi mode");
-//
-//	/*
-//	 * bits per word
-//	 */
-//	ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
-//	if (ret == -1)
-//		pabort("can't set bits per word");
-//
-//	ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
-//	if (ret == -1)
-//		pabort("can't get bits per word");
-//
-//	/*
-//	 * max speed hz
-//	 */
-//	ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
-//	if (ret == -1)
-//		pabort("can't set max speed hz");
-//
-//	ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
-//	if (ret == -1)
-//		pabort("can't get max speed hz");
-//
-//	printf("spi mode: %d\n", mode);
-//	printf("bits per word: %d\n", bits);
-//	printf("max speed: %d Hz (%d KHz)\n", speed, speed/1000);
-//
-//	transfer(fd);
-//
-//	close(fd);
-//
-//	return ret;
-//}
